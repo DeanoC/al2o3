302a4471f7305d44571df002cbd050e94992bf98
Dean Calver <me@deanoc.com>
2019-07-14 12:37:26 +0300

    TheForge SwapChain api implemented via C API(tests on OS X only) Devon uses the forge to display a yellow swapchain on OS X ADD_GUI_APP working now for AL2O3

--- cmake_modules/al2o3_app_utils.cmake
+++ cmake_modules/al2o3_app_utils.cmake
@@ -16,28 +16,43 @@ MACRO(ADD_CONSOLE_APP AppName Src Deps)
 	endif ()
 ENDMACRO()
 
-MACRO(ADD_GUI_APP AppName Src Deps)
+MACRO(ADD_GUI_APP AppName Src Deps ShellInterface)
 	set(_src ${Src})
 	set(_deps ${Deps} )
-
-	if(APPLE)
-		list(APPEND _src ${LIB_BASE_PATH}/level0/guishell/src/apple/appdelegate.h)
-		list(APPEND _src ${LIB_BASE_PATH}/level0/guishell/src/apple/appdelegate.m)
-		list(APPEND _src ${LIB_BASE_PATH}/level0/guishell/src/apple/macresources/MainMenu.nib)
-	endif()
-
-	add_executable(${AppName}  WIN32 MACOSX_BUNDLE ${_src})
-	target_link_libraries(${AppName} ${LibName} core guishell)
+	add_executable(${AppName} WIN32 MACOSX_BUNDLE ${_src} ${_headers})
+	target_link_libraries(${AppName} PRIVATE ${LibName})
 	foreach (_dep ${_deps})
 		get_filename_component(deplibname ${_dep} NAME)
-		target_link_libraries(${AppName} ${deplibname})
+		FETCH_DEPENDENCY(${deplibname})
+		target_link_libraries(${AppName} PRIVATE ${deplibname})
 	endforeach ()
-	if(APPLE)
+
+	if (APPLE)
+		if( NOT "Info.plist.in" IN_LIST Src)
+			configure_file(${${ShellInterface}_SOURCE_DIR}/default_macresources/Info.plist.in
+					${CMAKE_CURRENT_BINARY_DIR}/Info.plist.in COPYONLY)
+		endif()
+		set(_src_mainmenu_xib ${${ShellInterface}_SOURCE_DIR}/default_macresources/MainMenu.xib)
+
+		if(  "MainMenu.xib" IN_LIST Tests)
+			set(_src_mainmenu_xib ${CMAKE_CURRENT_SOURCE_DIR}/Src/MainMenu.xib)
+		endif()
+		target_sources(${AppName} PRIVATE MainMenu.nib)
+
+		add_custom_command(
+				OUTPUT MainMenu.nib
+				COMMAND ${IBTOOL} --output-format binary1 --compile MainMenu.nib ${_src_mainmenu_xib}
+		)
+
+		if( NOT "appdelegate.h" IN_LIST Tests)
+			target_sources(${AppName} PRIVATE ${${ShellInterface}_SOURCE_DIR}/default_macresources/appdelegate.h)
+			target_sources(${AppName} PRIVATE ${${ShellInterface}_SOURCE_DIR}/default_macresources/appdelegate.m)
+		endif()
+		target_link_libraries(${AppName} PRIVATE stdc++ "-framework Foundation" "-framework Cocoa" objc)
 		set_target_properties(${AppName} PROPERTIES
-				MACOSX_BUNDLE_GUI_IDENTIFIER com.wryd.${AppName}
-				MACOSX_BUNDLE_INFO_PLIST ${LIB_BASE_PATH}/level0/guishell/src/apple/macresources/Info.plist.in
-				RESOURCE ${LIB_BASE_PATH}/level0/guishell/src/apple/macresources/MainMenu.nib
+				MACOSX_BUNDLE_GUI_IDENTIFIER com.al2o3.${AppName}
+				MACOSX_BUNDLE_INFO_PLIST ${CMAKE_CURRENT_BINARY_DIR}/Info.plist.in
+				RESOURCE MainMenu.nib
 				)
 	endif()
-
 ENDMACRO()
