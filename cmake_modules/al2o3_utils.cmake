5d71aa8a4e8cfcb2ee16669ec76cb6afa92152b9
DeanoC <deano@cloudpixies.com>
2019-09-28 19:15:03 +0300

    Update al2o3_utils.cmake

--- cmake_modules/al2o3_utils.cmake
+++ cmake_modules/al2o3_utils.cmake
@@ -1,6 +1,6 @@
 MACRO(SET_MIN_VERSIONS)
 
-	cmake_minimum_required(VERSION 3.13 FATAL_ERROR)
+	cmake_minimum_required(VERSION 3.12.4 FATAL_ERROR)
 	# Fix behavior of CMAKE_CXX_STANDARD when targeting macOS.
 	if (POLICY CMP0025)
 		cmake_policy(SET CMP0025 NEW)
