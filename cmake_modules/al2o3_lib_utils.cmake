c76345ea36f58cf63c3657aaac441f50862b78f4
Deano <me@deanoc.com>
2019-10-13 17:02:58 +0300

    New lib2 al2o3 for glob'ed library (testing whether to swap over to it) Removed atomics from Os. TODO al2o3_os versions Implementation and tests of al2o3_handle system.

--- cmake_modules/al2o3_lib_utils.cmake
+++ cmake_modules/al2o3_lib_utils.cmake
@@ -44,4 +44,46 @@ MACRO(ADD_LIB LibName Headers Src Deps)
 	endif()
 ENDMACRO()
 
+# eventually this will be renamed to ADD_LIB and replace the only behaviour
+MACRO(ADD_LIB2 LibName Src Deps)
+	set(_deps ${Deps})
+	set(_srcs ${Src})
+
+	if(DEFINED _srcs)
+		list(LENGTH _srcs _SrcLength)
+	endif()
+
+	if(_SrcLength)
+		add_library( ${LibName} ${_srcs} )
+
+		foreach (_dep ${_deps})
+			get_filename_component(deplibname ${_dep} NAME)
+			FETCH_DEPENDENCY(${deplibname})
+			target_link_libraries(${LibName} PUBLIC ${deplibname})
+		endforeach ()
+
+		target_include_directories( ${LibName}
+				PUBLIC
+				${CMAKE_CURRENT_SOURCE_DIR}/include
+				PRIVATE
+				${CMAKE_CURRENT_SOURCE_DIR}/src )
+	else()
+		add_library( ${LibName} INTERFACE )
+
+		foreach (_dep ${_deps})
+			get_filename_component(deplibname ${_dep} NAME)
+			FETCH_DEPENDENCY(${deplibname})
+			target_link_libraries(${LibName} INTERFACE ${deplibname})
+		endforeach ()
+
+		target_include_directories( ${LibName}
+				INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/include )
+	endif()
+	if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE)
+		configure_file(${CMAKE_CURRENT_SOURCE_DIR}/LICENSE
+				${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}/${LibName}_LICENSE COPYONLY)
+	endif()
+
+ENDMACRO()
+
 
